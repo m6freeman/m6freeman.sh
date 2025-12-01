f26399127cb87ac9b6cddeb5d2b450bf50dbaca6
Matt Freeman <m6freeman@tuta.io>
2025-11-30 19:07:26 -0500

    fix: bucket policy dependency

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -11,6 +11,8 @@ resource "aws_s3_bucket_public_access_block" "website_bucket" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+
+  depends_on = [aws_s3_bucket_policy.website_bucket]
 }
 
 resource "aws_s3_bucket_ownership_controls" "website_bucket" {
