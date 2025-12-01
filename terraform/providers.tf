073e2100f5ed9cc9889357935b4b4f7a57e732cc
Matt Freeman <m6freeman@tuta.io>
2025-11-30 19:11:01 -0500

    fix: removing localhost endpoints; needs proper solution

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = local.config.project.region
-  dynamic "endpoints" {
-    for_each = terraform.workspace == "localstack" ? [1] : [0]
-    content {
-      s3  = local.config.infrastructure.localstack_endpoint
-      sts = local.config.infrastructure.localstack_endpoint
-    }
-  }
-  skip_credentials_validation = false
-  skip_requesting_account_id  = false
+  # dynamic "endpoints" {
+  # for_each = terraform.workspace == "localstack" ? [1] : [0]
+  # content {
+  # s3  = local.config.infrastructure.localstack_endpoint
+  # sts = local.config.infrastructure.localstack_endpoint
+  # }
+  # }
+  skip_credentials_validation = terraform.workspace == "localstack"
+  skip_requesting_account_id  = terraform.workspace == "localstack"
 }
