c7f0eeb593bdbdd336df89116a99c2b7431ae441
Matt Freeman <m6freeman@tuta.io>
2025-12-21 16:46:35 -0500

    examples page building from index.md [partial]

--- src/components/examples.py
+++ src/components/examples.py
@@ -1,3 +1,4 @@
+import re
 from rich.console import Group
 from rich.syntax import Syntax
 from rich.text import Text
@@ -7,29 +8,38 @@ from components import hampton
 from components import pilot
 from components import usn
 from components import web_dev
+from page_renderer import get_analyzer
 
 
-def build() -> Group:
+def build(full_md: str) -> Group:
 
-    title: Text = Text("EXAMPLES", style="title_text")
+    examples_pattern: str = b'(?s)(##\\s*EXAMPLES\\s*\\n.*?)(?=\\n##\\s|$)'
+    examples_match: re.match = re.search(
+        examples_pattern, full_md.encode())
+    examples_md: str = examples_match.group(1).decode()
+    examples_analyzer = get_analyzer(examples_md)
 
-    most_relevant_syntax: Syntax = Syntax("""
-$ mf --serverless_developer aws [dotnet, python] github_actions terraform
-$ mf --software_engineer c# dotnet enterprise_solutions
-        """, lexer="bash")
+    header: str = examples_analyzer.identify_headers()\
+        .get('Header')[0].get('text')
 
-    historical_syntax: Syntax = Syntax("""
-$ mf --enthusiast
-$ mf --game_programmer
-$ mf --professional  | grep 'history'
+    title: Text = Text(header, style="title_text")
+
+    most_relevant_syntax: Syntax = Syntax(f"""
+$ {examples_analyzer.identify_inline_code()[0].get('code')}
+$ {examples_analyzer.identify_inline_code()[1].get('code')}
+        """, lexer="bash", word_wrap=True)
+
+    historical_syntax: Syntax = Syntax(f"""
+{'\n'.join("$ " + example_syntax.get('code', "") for example_syntax
+           in examples_analyzer.identify_inline_code()[2:])}
         """, lexer="bash")
 
     return Group(
         title, "\n",
         most_relevant_syntax, "\n",
-        pilot.build(), "\n",
+        pilot.build(full_md), "\n",
         historical_syntax, "\n",
-        ciat.build(), "\n",
+        ciat.build(full_md), "\n",
         game_dev.build(), "\n",
         usn.build(), "\n",
         web_dev.build(), "\n",
