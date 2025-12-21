54533e495d28e7d5595837cd795bde009d2cfa43
Matt Freeman <m6freeman@tuta.io>
2025-12-21 16:46:52 -0500

    options page built from index.md

--- src/components/options.py
+++ src/components/options.py
@@ -1,68 +1,53 @@
+import re
 from rich.console import Group
 from rich.panel import Panel
 from rich.padding import Padding
 from rich.text import Text
+from page_renderer import get_analyzer
 import rich.box
 
 
-def build() -> Group:
+def build(full_md: str) -> Group:
 
-    enthusiast_bq: Text = Text.assemble(
-        "Enthusiastic about technologies with a heavy emphasis towards \
-privacy, security, transparency, and decentralization.", style="blockquote")
-    enthusiast_bq: Padding = Padding(
-        enthusiast_bq, (0, 4))
+    options_pattern: str = b'(?s)(##\\s*OPTIONS\\s*\\n.*?)(?=\\n##\\s|$)'
+    options_match: re.match = re.search(
+        options_pattern, full_md.encode())
+    options_md: str = options_match.group(1).decode()
+    options_analyzer = get_analyzer(options_md)
 
-    game_programmer_bq: Text = Text.assemble(
-        "Developed and published Android and Windows platform games with ",
-        ("Unity3D", "cyan"), ", ",
-        ("C#", "cyan"), ", ",
-        (".NET Framework", "cyan"), ", and ",
-        ("Visual Studio", "cyan"),
-        style="blockquote"
-    )
-    game_programmer_bq: Padding = Padding(
-        game_programmer_bq, (0, 4))
+    header: str = options_analyzer.identify_headers()\
+        .get('Header')[0].get('text')
 
-    professional_bq: Text = Text.assemble(
-        "Collaborates with all levels of personnel, senior leadership, \
-and clients to foster continuous improvement and enhance profitability.",
-        style="blockquote")
-    professional_bq: Padding = Padding(professional_bq, (0, 4))
+    options: list[str] = [
+        code.get('code') for code
+        in options_analyzer.identify_inline_code()
+        if code.get('code').startswith('--')
+    ]
 
-    serverless_developer_bq: Text = Text.assemble(
-        "Designs, develops, and deploys scalable enterprise solutions within ",
-        ("AWS", "cyan"), " using ",
-        ("API Gateway", "cyan"), ", ",
-        ("DynamoDb", "cyan"), ", ",
-        ("EC2", ""), ", ",
-        ("Lambda", "cyan"), ", ",
-        ("S3", "cyan"), ", ",
-        ("SNS", "cyan"), ", and ",
-        ("SQS", "cyan"),
-        style="blockquote"
-    )
-    serverless_developer_bq: Padding = Padding(
-        serverless_developer_bq, (0, 4))
+    techs: list[str] = [
+        code.get('code') for code
+        in options_analyzer.identify_inline_code()
+        if not code.get('code').startswith('--')
+    ]
 
-    software_engineer_bq: Text = Text.assemble(
-        "Introduce new and modify existing features, perform debugging and \
-patching, and perform project and infrastructural upgrades to legacy \
-enterprise Financial, Payroll, HR, and Benefits systems", style="blockquote")
-    software_engineer_bq: Padding = Padding(software_engineer_bq, (0, 4))
+    blockquotes: list[str] = [
+        Padding(Text.assemble(
+            *((word, "cyan") if word in techs else word
+                for word in blockquote.split('`')),
+            style="blockquote"), (0, 4))
+        for blockquote in
+        options_analyzer.identify_blockquotes().get('Blockquote')
+    ]
 
     return Group(
-        Text("OPTIONS", style="title_text"),
+        Text(header, style="title_text"),
         "\n",
-        Text("--enthusiast", style="bold"),
-        Panel(professional_bq, box=rich.box.SIMPLE, border_style="none"),
-        Text("--game_programmer", style="bold"),
-        Panel(game_programmer_bq, box=rich.box.SIMPLE, border_style="none"),
-        Text("--professional", style="bold"),
-        Panel(professional_bq, box=rich.box.SIMPLE, border_style="none"),
-        Text("--serverless_developer", style="bold"),
-        Panel(serverless_developer_bq,
-              box=rich.box.SIMPLE, border_style="none"),
-        Text("--software_engineer", style="bold"),
-        Panel(software_engineer_bq, box=rich.box.SIMPLE, border_style="none"),
+        *(
+            text_or_panel
+            for option, blockquote in zip(options, blockquotes)
+            for text_or_panel in [
+                Text(option, style="bold"),
+                Panel(blockquote, box=rich.box.SIMPLE, border_style="none")
+            ]
+        ),
     )
