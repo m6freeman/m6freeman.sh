9afa69117ffd1653c2c35753173e69053e48c9d5
Matt Freeman <m6freeman@tuta.io>
2025-12-21 16:45:50 -0500

    desc page built from index.md

--- src/components/description.py
+++ src/components/description.py
@@ -1,19 +1,27 @@
+import re
 from rich.console import Group
 from rich.text import Text
 from rich.padding import Padding
+from page_renderer import get_analyzer
 
 
-def build() -> Group:
+def build(full_md: str) -> Group:
+
+    desc_pattern: str = b'(?s)(##\\s*DESCRIPTION\\s*\\n.*?)(?=\\n##\\s|$)'
+    desc_match: re.match = re.search(
+        desc_pattern, full_md.encode())
+    desc_md: str = desc_match.group(1).decode()
+    desc_analyzer = get_analyzer(desc_md)
+
+    header: str = desc_analyzer.identify_headers().get('Header')[0].get('text')
 
     return Group(
-        Text("DESCRIPTION", style="title_text"),
+        Text(header, style="title_text"),
         "\n",
         Padding(
             Group(
-                Text("Actively develops event-driven, serverless enterprise solutions while supporting legacy corporate infrastructure."),
-                "\n",
-                Text(
-                    "Strong technical background encompassing a variety of software and hardware technologies")
+                *(Text(paragraph + '\n\n') for paragraph
+                  in desc_analyzer.identify_paragraphs().get('Paragraph'))
             ), (0, 2)
         ),
     )
