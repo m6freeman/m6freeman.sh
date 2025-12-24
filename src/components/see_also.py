eada65902bf19e09d18e395d747a83a94c0edb50
Matt Freeman <m6freeman@tuta.io>
2025-12-24 17:49:46 -0500

    read data from index.md

--- src/components/see_also.py
+++ src/components/see_also.py
@@ -1,34 +1,54 @@
-import json
+import re
 import rich.box
+from page_renderer import get_analyzer
 from rich.console import Group
 from rich.table import Table
 from rich.text import Text
 
 
-def build(contacts_path: str) -> Group:
+def build(full_md: str) -> Group:
 
-    with open(contacts_path, "r", encoding="utf-8") as f:
-        contacts: list[dict[str, str]] = json.load(f)
+    refs_pattern: str = b'(?s)(##\\s*SEE\\sALSO\\s*\\n.*?)(?=\\n##\\s|$)'
+    refs_match: re.match = re.search(
+        refs_pattern, full_md.encode())
+    refs_md: str = refs_match.group(1).decode()
+    refs_analyzer = get_analyzer(refs_md)
+
+    header: str = refs_analyzer.identify_headers()\
+        .get('Header')[0].get('text')
 
     t: Table = Table(box=rich.box.SIMPLE_HEAD)
-    t.add_column("Employer", justify="left", style="blue", width=8)
-    t.add_column("Role", justify="left", width=8)
-    t.add_column("Name", justify="left")
-    t.add_column("Phone", justify="right", style="blue")
-    t.add_column("Email", justify="right", style="blue")
-    for contact in contacts:
-        t.add_row(
-            contact.get("employer", ""),
-            contact.get("role", ""),
-            contact.get("name", ""),
-            contact.get("phone", ""),
-            contact.get("email", "")
-        )
+    col_headers: list[str] = refs_analyzer.identify_tables().get('Table')[
+        0].get('header')
+    t.add_column(col_headers[0], justify="left", style="blue", width=8)
+    t.add_column(col_headers[1], justify="left", width=8)
+    t.add_column(col_headers[2], justify="left")
+    t.add_column(col_headers[3], justify="right", style="blue")
+    t.add_column(col_headers[4], justify="right", style="blue")
+    for row in refs_analyzer.identify_tables().get('Table')[0].get('rows'):
+        try:
+            emp: str = re.search(b'\\[([^\\]]+)\\]',
+                                 row[0].encode()).group(1).decode()
+        except Exception:
+            emp = ""
+        role: str = row[1] or ""
+        name: str = row[2] or ""
+        try:
+            phone: str = re.search(
+                b'\\[([^\\]]+)\\]', row[3].encode()).group(1).decode()
+        except Exception:
+            phone = ""
+        try:
+            email: str = re.search(b'\\[([^\\]]+)\\]', row[4].encode()
+                                   ).group(1).decode().replace('@', '\n@')
+        except Exception:
+            email = ""
+        t.add_row(emp, role, name, phone, email)
         t.add_row()
     professional_references_table: Table = t
 
     return Group(
-        Text("SEE ALSO", style="title_text"),
+        Text(header, style="title_text"),
         "\n",
         professional_references_table
     )
